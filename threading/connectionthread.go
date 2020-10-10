2c89bf846e2829c4adb354ef52a334967f3ba5fc
SparklyCatTF2 <32608971+SparklyCatTF2@users.noreply.github.com>
2020-10-10 22:01:32 +0300

    Add thread to keep connection to economy.roblox.com open

---
+++ threading/connectionthread.go
@@ -0,0 +1,23 @@
+package threading
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	"github.com/SparklyCatTF2/Reaper/rblx"
+)
+
+// ConnectionThread keeps the connection to economy.roblox.com open on a *rblx.RBLXSession instance
+func ConnectionThread(snipeAccountSession *rblx.RBLXSession) {
+	for {
+		req, _ := http.NewRequest("GET", "https://economy.roblox.com/v1/resale-tax-rate", nil)
+		req.Header.Add("Cookie", ".ROBLOSECURITY="+snipeAccountSession.Cookie)
+		resp, respErr := snipeAccountSession.Client.Do(req)
+		if respErr != nil { continue }
+		resp.Body.Close()
+
+		time.Sleep(5 * time.Second)
+	}
+}
